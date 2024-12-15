c75e68d1516b57f7df31df569e8762dbb824ac9f
actuponit <imo60067035@gmail.com>
2024-12-15 09:18:33 +0300

    change localhost to the server

--- client/utils/displayImage.ts
+++ client/utils/displayImage.ts
@@ -1,5 +1,5 @@
 export default function displayImage(url: string | null | undefined) {
   if(!url)
     return "https://placehold.co/400"
-  return "http://localhost:3000/"+url
+  return "https://my-cinema-1.onrender.com"+url
 }
